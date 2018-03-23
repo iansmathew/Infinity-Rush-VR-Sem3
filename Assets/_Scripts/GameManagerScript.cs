d81d5adceed0d831d0262dfe9a3916ade3f387b4
Ian Sebastian Mathew <iansmathew@gmail.com>
2018-03-23 15:28:39 -0400

    Added menu scene
    Added menu on start and game manager class.

---
+++ Assets/_Scripts/GameManagerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+
+    [Header("Managers")]
+    [SerializeField]
+    private GameObject enemyManager;
+    [SerializeField]
+    private GameObject player;
+
+    //Instance
+    public static GameManagerScript Instance { get; private set; }
+
+    private void Start()
+    {
+        if (Instance != null)
+            Destroy(gameObject);
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public void StartGame()
+    {
+        enemyManager.GetComponent<EnemySpawn>().enabled = true;
+        player.GetComponent<PlayerFuelScript>().enabled = true;
+    }
+}
